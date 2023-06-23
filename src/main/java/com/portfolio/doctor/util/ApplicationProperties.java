450a045f039c7ef3ce610aaa6cd52b8ac57f8e0b
Shohruxjan <shox1196@gmail.com>
2023-06-23 05:05:45 +0500

    2nd Milestone. Initial commit

--- src/main/java/com/portfolio/doctor/util/ApplicationProperties.java
+++ src/main/java/com/portfolio/doctor/util/ApplicationProperties.java
@@ -11,6 +11,7 @@ import java.util.List;
 @ConfigurationProperties(prefix = "api")
 public class ApplicationProperties {
     private List<String> keys;
+    private String url;
     private Integer index = 0;
 
     public List<String> getKeys() {
