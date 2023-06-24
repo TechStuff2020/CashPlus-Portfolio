133cafdcc15f9779bb0fd62df9f849df1be104cd
Shohruxjan <shox1196@gmail.com>
2023-06-24 14:07:16 +0500

    added cross origin

--- src/main/java/com/portfolio/doctor/controller/PortfolioController.java
+++ src/main/java/com/portfolio/doctor/controller/PortfolioController.java
@@ -6,14 +6,12 @@ import com.portfolio.doctor.service.PortfolioService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/portfolio")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class PortfolioController {
 
     private final PortfolioService portfolioService;
